d6631a67018320848c1e0472fa3afed9d133bb76
guoziA <justforfr@163.com>
2016-09-28 22:04:28 +0800

    tulun

---
+++ 凸轮生成/凸轮生成/Form1.Designer.vb
@@ -0,0 +1,369 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.RadioButton5 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(34, 335)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(60, 16)
+        Me.CheckBox1.TabIndex = 0
+        Me.CheckBox1.Text = "逆时针"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(32, 36)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(41, 12)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "升程h:"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(982, 647)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(133, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "生成"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.SystemColors.Window
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 48)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(591, 622)
+        Me.PictureBox1.TabIndex = 3
+        Me.PictureBox1.TabStop = False
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(633, 12)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.ReadOnly = True
+        Me.RichTextBox1.Size = New System.Drawing.Size(284, 658)
+        Me.RichTextBox1.TabIndex = 4
+        Me.RichTextBox1.Text = ""
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(100, 33)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(67, 21)
+        Me.TextBox1.TabIndex = 5
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.GroupBox2)
+        Me.GroupBox1.Controls.Add(Me.CheckBox2)
+        Me.GroupBox1.Controls.Add(Me.Label7)
+        Me.GroupBox1.Controls.Add(Me.TextBox7)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.TextBox5)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.TextBox6)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.TextBox4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.TextBox3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.CheckBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(948, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(210, 620)
+        Me.GroupBox1.TabIndex = 6
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "参数"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.RadioButton5)
+        Me.GroupBox2.Controls.Add(Me.RadioButton4)
+        Me.GroupBox2.Controls.Add(Me.RadioButton3)
+        Me.GroupBox2.Controls.Add(Me.RadioButton2)
+        Me.GroupBox2.Controls.Add(Me.RadioButton1)
+        Me.GroupBox2.Location = New System.Drawing.Point(19, 415)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(173, 196)
+        Me.GroupBox2.TabIndex = 7
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "运动规律"
+        '
+        'RadioButton5
+        '
+        Me.RadioButton5.AutoSize = True
+        Me.RadioButton5.Location = New System.Drawing.Point(15, 174)
+        Me.RadioButton5.Name = "RadioButton5"
+        Me.RadioButton5.Size = New System.Drawing.Size(41, 16)
+        Me.RadioButton5.TabIndex = 11
+        Me.RadioButton5.TabStop = True
+        Me.RadioButton5.Text = "sin"
+        Me.RadioButton5.UseVisualStyleBackColor = True
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Location = New System.Drawing.Point(15, 136)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(41, 16)
+        Me.RadioButton4.TabIndex = 10
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.Text = "cos"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(15, 105)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(59, 16)
+        Me.RadioButton3.TabIndex = 9
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "多项式"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(15, 71)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(59, 16)
+        Me.RadioButton2.TabIndex = 8
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "等加速"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(15, 33)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(47, 16)
+        Me.RadioButton1.TabIndex = 7
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "等速"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(34, 379)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(84, 16)
+        Me.CheckBox2.TabIndex = 18
+        Me.CheckBox2.Text = "从动件在右"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(32, 123)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(59, 12)
+        Me.Label7.TabIndex = 16
+        Me.Label7.Text = "滚子半径:"
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(100, 120)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(67, 21)
+        Me.TextBox7.TabIndex = 17
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(32, 292)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(47, 12)
+        Me.Label6.TabIndex = 14
+        Me.Label6.Text = "回程角:"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(100, 245)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(67, 21)
+        Me.TextBox5.TabIndex = 13
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(32, 248)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(47, 12)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "远停角:"
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(100, 289)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(67, 21)
+        Me.TextBox6.TabIndex = 15
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(32, 205)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(47, 12)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "升程角:"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(100, 202)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(67, 21)
+        Me.TextBox4.TabIndex = 11
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(32, 78)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(71, 12)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "基圆半径r0:"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(100, 75)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(67, 21)
+        Me.TextBox3.TabIndex = 9
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(32, 163)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 12)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "偏心距e:"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(100, 160)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(67, 21)
+        Me.TextBox2.TabIndex = 7
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(12, 24)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(341, 12)
+        Me.Label8.TabIndex = 7
+        Me.Label8.Text = "说明：参数列表中，角度单位为度，显示图形大小尺寸经过放大"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1184, 685)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "凸轮生成"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents CheckBox2 As System.Windows.Forms.CheckBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents TextBox7 As System.Windows.Forms.TextBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents TextBox6 As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents RadioButton5 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton4 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton3 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton2 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton1 As System.Windows.Forms.RadioButton
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+
+End Class
---
+++ 凸轮生成/凸轮生成/Form1.vb
@@ -0,0 +1,239 @@
+﻿Public Class Form1
+
+    Private Const CONSTV = 0
+    Private Const ACC = 1
+    Private Const MULTINOMIAL = 2
+    Private Const COS = 3
+    Private Const SIN = 4
+
+    Private Const ANGLE_STEP = 2 * Math.PI / 36
+    Private Const PI2 = 2 * Math.PI
+
+    Dim motionLaw As Integer
+    Dim h1 As Double
+    Dim s0 As Double
+    Dim r0 As Double
+    Dim rr As Double
+    Dim e0 As Double
+    Dim angle_push As Double
+    Dim angle_far As Double
+    Dim angle_back As Double
+
+    Dim x As Double
+    Dim y As Double
+
+    Dim x0 As Double
+    Dim y0 As Double
+
+    Dim xb As Single
+    Dim yb As Single
+
+    Dim s As Double
+
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        Dim r As Double
+
+        Dim bm As New Bitmap(591, 658)
+        Dim camGraphics As Graphics = Graphics.FromImage(bm)
+
+
+        Try
+            h1 = TextBox1.Text
+            e0 = TextBox2.Text
+            r0 = TextBox3.Text
+            angle_push = TextBox4.Text * Math.PI / 180
+            angle_far = TextBox5.Text * Math.PI / 180
+            angle_back = TextBox6.Text * Math.PI / 180
+            rr = TextBox7.Text
+
+            '判断e0的正负
+            e0 = getE()
+            '获得运动规律
+            motionLaw = mLaw()
+
+            If motionLaw = MULTINOMIAL Then
+                MessageBox.Show("还没做！")
+                Return
+            End If
+
+            '计算s0
+            s0 = Math.Sqrt(Math.Pow(r0, 2) + Math.Pow(e0, 2))
+
+            RichTextBox1.Text = ""
+            PictureBox1.Image = Nothing
+
+            For angle = 0 To PI2 Step ANGLE_STEP
+
+                Call getXY(rr, angle)
+                If angle > 0 Then
+
+                    r = Math.Sqrt(Math.Pow(xb, 2) + Math.Pow(yb, 2)) '到原点距离
+
+                    'RichTextBox1.AppendText("angle = " & Format(angle * 180 / Math.PI, "###0.000") & ", r = " & Format(r, "###0.000") & Chr(13))
+
+                    '放大
+                    r = 4 * r
+
+                    Dim px As Single
+                    px = r * Math.Cos(angle) + 260
+                    Dim py As Single
+                    py = r * Math.Sin(angle) + 290
+
+                    camGraphics.FillRectangle(Brushes.Black, px, py, 2.0F, 2.0F)
+                End If
+                PictureBox1.Image = bm
+            Next
+            camGraphics.FillEllipse(Brushes.Black, 260.0F, 290.0F, 5.0F, 5.0F)
+            PictureBox1.Image = bm
+        Catch ex As Exception
+            MessageBox.Show("请输入数值！")
+        End Try
+
+
+    End Sub
+
+    Private Sub getXY(ByVal rr As Double, ByVal angle As Double)        '计算b点轮廓线
+
+        getTheoreticalXY(angle, False)
+
+        If angle > 0 Then
+            Dim dx, dy, factorx, factory As Double
+
+            getTheoreticalXY(angle - ANGLE_STEP, True)
+
+            dx = Math.Abs(x - x0)
+            dy = Math.Abs(y - y0)
+
+            factorx = rr * (dy / ANGLE_STEP) / Math.Sqrt(Math.Pow(dx / ANGLE_STEP, 2) + Math.Pow(dy / ANGLE_STEP, 2))
+            factory = rr * (dx / ANGLE_STEP) / Math.Sqrt(Math.Pow(dx / ANGLE_STEP, 2) + Math.Pow(dy / ANGLE_STEP, 2))
+
+            If (Math.PI / 2) < angle And angle <= (Math.PI) Then
+                xb = x - factorx
+                yb = y + factory
+            ElseIf (Math.PI) < angle And angle <= (3 * Math.PI / 2) Then
+                xb = x + factorx
+                yb = y + factory
+            ElseIf (3 * Math.PI / 2) < angle And angle < (PI2) Then
+                xb = x + factorx
+                yb = y - factory
+            Else
+                xb = x - factorx
+                yb = y - factory
+
+            End If
+        End If
+
+    End Sub
+
+    Private Sub getTheoreticalXY(ByVal angle As Double, ByVal ispre_x As Boolean)     '计算理论轮廓线
+
+        s = getS(motionLaw, angle, h1, angle_push, angle_far, angle_back)
+
+        If ispre_x Then
+            x0 = (s + s0) * Math.Sin(angle) + e0 * Math.Cos(angle)
+            y0 = (s + s0) * Math.Cos(angle) - e0 * Math.Sin(angle)
+
+        Else
+            x = (s + s0) * Math.Sin(angle) + e0 * Math.Cos(angle)
+            y = (s + s0) * Math.Cos(angle) - e0 * Math.Sin(angle)
+            RichTextBox1.AppendText(angle * 180 / Math.PI & "    " & s & Chr(13))
+        End If
+
+    End Sub
+
+    Private Function mLaw() As Integer     '选择运动规律
+
+        If RadioButton1.Checked Then
+            mLaw = CONSTV
+        ElseIf RadioButton2.Checked Then
+            mLaw = ACC
+        ElseIf RadioButton3.Checked Then
+            mLaw = MULTINOMIAL
+        ElseIf RadioButton4.Checked Then
+            mLaw = COS
+        Else
+            mLaw = SIN
+
+        End If
+    End Function
+
+    Private Function getE() As Double  '判断e的正负
+        If (CheckBox1.Checked And CheckBox2.Checked) Or ((Not CheckBox1.Checked) And (Not CheckBox2.Checked)) Then
+            getE = Math.Abs(e0)
+        Else
+            getE = -Math.Abs(e0)
+        End If
+
+    End Function
+
+    Private Function getS(ByRef MotionLaw As Integer, ByRef angle As Double, ByRef h As Double,
+                     ByRef angle_p As Double, ByRef angle_f As Double, ByRef angle_b As Double) As Double  '计算s
+
+        Select Case MotionLaw
+
+            Case CONSTV
+                '等速
+                If 0 < angle And angle <= angle_p Then
+                    getS = h * angle / angle_p
+
+                ElseIf angle_p < angle And angle <= (angle_b + angle_far) Then
+                    getS = h
+
+                ElseIf (angle_p + angle_f) < angle And angle <= (angle_p + angle_f + angle_b) Then
+                    getS = h - h * (angle - (angle_p + angle_f)) / angle_b
+
+                Else
+                    getS = 0
+
+                End If
+            Case ACC
+                '等加速度
+                If 0 < angle And angle <= angle_p Then
+                    getS = 2 * h * Math.Pow(angle, 2) / Math.Pow(angle_p, 2)
+
+                ElseIf angle_p < angle And angle <= (angle_b + angle_far) Then
+                    getS = h
+
+                ElseIf (angle_p + angle_f) < angle And angle <= (angle_p + angle_f + angle_b) Then
+                    getS = h - 2 * h * Math.Pow((angle - (angle_p + angle_f)), 2) / Math.Pow(angle_p, 2)
+
+                Else
+                    getS = 0
+                End If
+            Case MULTINOMIAL
+                getS = 0        '多项式 不写】
+            Case COS
+                '余弦
+                If 0 < angle And angle <= angle_p Then
+                    getS = h / 2 - h * Math.Cos(Math.PI * angle / angle_p) / 2
+
+                ElseIf angle_p < angle And angle <= (angle_b + angle_far) Then
+                    getS = h
+
+                ElseIf (angle_p + angle_f) < angle And angle <= (angle_p + angle_f + angle_b) Then
+                    getS = h / 2 + h * Math.Cos(Math.PI * (angle - (angle_p + angle_f)) / angle_b) / 2
+
+                Else
+                    getS = 0
+                End If
+            Case Else
+                '正弦
+                If 0 < angle And angle <= angle_p Then
+                    getS = h * angle / angle_p - (h * Math.Sin(PI2 * angle / angle_p)) / PI2
+
+                ElseIf angle_p < angle And angle <= (angle_b + angle_far) Then
+                    getS = h
+
+                ElseIf (angle_p + angle_f) < angle And angle <= (angle_p + angle_f + angle_b) Then
+                    getS = h - h * (angle - (angle_p + angle_f)) / angle_b + h * Math.Sin(PI2 * (angle - (angle_p + angle_f))) / PI2
+
+                Else
+                    getS = 0
+
+                End If
+        End Select
+
+    End Function
+End Class
